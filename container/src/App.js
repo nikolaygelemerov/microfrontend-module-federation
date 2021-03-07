dc29a3df06d4f67340f44fe41ca42a739e099098
nikolaygelemerov <nikolaygelemerov@gmail.com>
2021-03-07 19:09:21 +0200

    initial commit

---
+++ container/src/App.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import MarketingApp from './components/MarketingApp';
+
+export default () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log(counter);
+  }, []);
+
+  return (
+    <div>
+      <h1>Hi There!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
---
+++ marketing/config/webpack.common.js
@@ -0,0 +1,20 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // '@babel/preset-react' - transforms react code (jsx)
+            // '@babel/preset-env' - transforms ES versions to latest browser support
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            // '@babel/plugin-transform-runtime' - enables some ES features in browsers (async await for example)
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    ]
+  }
+};
