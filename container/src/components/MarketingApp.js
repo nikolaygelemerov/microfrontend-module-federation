03d1c07bb70a8a8c54ca37e4b55afbb403fbcc4f
nikolaygelemerov <nikolaygelemerov@gmail.com>
2021-03-19 12:30:48 +0200

    created container deploy script

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { mount } from 'marketing/MarketingApp';
 
 export default () => {
--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJSON = require('../package.json');
@@ -19,9 +18,6 @@ const devConfig = {
       filename: 'remoteEntry.js',
       exposes: { './MarketingApp': './src/bootstrap' },
       shared: packageJSON.dependencies
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
     })
   ]
 };
