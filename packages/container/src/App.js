4c5a15571ded68f460f31644897949a1b358df06
nikolaygelemerov <nikolaygelemerov@gmail.com>
2021-03-23 16:47:39 +0200

    handle production css styling issues

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,5 +1,18 @@
 import React, { useEffect, useState } from 'react';
+<<<<<<< Updated upstream
+=======
+import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName
+} from '@material-ui/core/styles';
+
+>>>>>>> Stashed changes
 import MarketingApp from './components/MarketingApp';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+});
 
 export default () => {
   const [counter, setCounter] = useState(0);
@@ -9,10 +22,21 @@ export default () => {
   }, []);
 
   return (
+<<<<<<< Updated upstream
     <div>
       <h1>Hi There!</h1>
       <hr />
       <MarketingApp />
     </div>
+=======
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+>>>>>>> Stashed changes
   );
 };
